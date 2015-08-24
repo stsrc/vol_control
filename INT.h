7a13202dc2180deb8ad058df5ce9ef170dd18851
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2015-08-24 15:20:30 +0200

    Interrupt functions as inlines

--- INT.h
+++ INT.h
@@ -4,9 +4,33 @@
 #include <stdio.h>
 #include <avr/io.h>
 
-uint8_t INT_as_rising_edge();
-void INT_turn_edge();
-void INT_on_falling_edge();
-void INT_on_rising_edge();
+inline uint8_t INT_as_rising_edge()
+{
+	if(MCUCR & _BV(ISC10))
+		return 1;
+	return 0;
+}
+
+inline void INT_turn_edge()
+{
+	MCUCR ^= _BV(ISC10);
+	MCUCR |= _BV(ISC11);
+}
+
+inline void INT_on_falling_edge()
+{
+	MCUCR |= _BV(ISC11);
+	MCUCR &= ~_BV(ISC10);
+}
+
+inline void INT_on_rising_edge()
+{
+	MCUCR |= _BV(ISC11) | _BV(ISC10);
+}
+
+inline void INT_init()
+{
+	MCUCR |= _BV(ISC11);	
+}
 
 #endif
