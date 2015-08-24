f0986e9607f9b13eafede96ccdf8a16150f240cd
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2015-08-25 00:24:10 +0200

    TODO: remove delays from rest of files. Complete debounce fun

--- buttons.c
+++ buttons.c
@@ -3,7 +3,8 @@
 struct BTN_struct {
 	const uint8_t cnt;
 	const uint8_t pins;
-	uint8_t pressed;	
+	uint8_t pressed;
+	uint8_t held;	
 };
 
 static struct BTN_struct btns = {
@@ -21,34 +22,40 @@ void BTN_init()
 	LED_ON();
 }
 
-static inline void BTN_check()
+static inline uint8_t BTN_check()
 {
-	btns.pressed = (~PIND) & btns.pins;
+	return ((~PIND) & btns.pins);
 }
 
 static inline void BTN_debounce()
 {
-	/*button debounce*/
-	for (uint8_t it = 1; it != 0; it++);
+		
 }
 
+/*Place for functions which buttons may trigger*/
 static inline void BTN_perform()
 {
-	if (_BV(BTN1) & btns.pressed)
+	if (_BV(BTN1) & btns.pressed & (~btns.held)) {
 		LED_OFF();
-	if (_BV(BTN2) & btns.pressed)
+		btns.pressed &= ~_BV(BTN1);
+	}
+	if (_BV(BTN2) & btns.pressed & (~btns.held)) {
 		LED_ON();
-	if (_BV(BTN3) & btns.pressed)
+		btns.pressed &= ~_BV(BTN2);
+	}
+	if (_BV(BTN3) & btns.pressed & (~btns.held)) {
 		LED_OFF();
+		btns.pressed &= ~_BV(BTN3);
+	}
 }
 
 void BTN_action()
 {
-	BTN_check();
+	btns.pressed |= BTN_check();
 	if (btns.pressed) {
 		BTN_debounce();
+		btns.held = BTN_check() & btns.pressed;
 		BTN_perform();
-		btns.pressed = 0;
 	}
 }
 
