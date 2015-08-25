e71ebf24b792a1fc39fa71fda6fecaef62bbd335
Konrad Gotfryd <gotfrydkonrad@gmail.com>
2015-08-25 13:04:38 +0200

    debounce fun. is ready

--- buttons.c
+++ buttons.c
@@ -1,22 +1,27 @@
 #include "buttons.h"
+#include <stdlib.h>
 
 struct BTN_struct {
-	const uint8_t cnt;
+	const uint8_t no;
 	const uint8_t pins;
-	uint8_t pressed;
-	uint8_t held;	
+	uint8_t *cntrs;
 };
 
 static struct BTN_struct btns = {
-	.cnt = 3,
-	.pins = _BV(BTN1) | _BV(BTN2) | _BV(BTN3)
+	.no = 3,
+	.pins = _BV(BTN1) | _BV(BTN2) | _BV(BTN3),
+	.cntrs = NULL
 };
 
 void BTN_init()
 {
 	DDRD &= ~btns.pins;
 	PORTD |= btns.pins;
-
+	btns.cntrs = malloc(sizeof(uint8_t) * btns.no);
+	if (btns.cntrs == NULL)
+		while(1);
+	for (uint8_t it = 0; it < btns.no; it++)
+		btns.cntrs[it] = 0;
 	//DEBUG
 	DDRD |= _BV(LED);
 	LED_ON();
@@ -27,35 +32,62 @@ static inline uint8_t BTN_check()
 	return ((~PIND) & btns.pins);
 }
 
-static inline void BTN_debounce()
+static inline uint8_t BTN_debounce()
 {
-		
+	uint8_t rt = 0;
+	uint8_t pressed = BTN_check();
+	if (btns.cntrs[0]) {
+		btns.cntrs[0]++;
+		if (!btns.cntrs[0]) {
+			if (BTN_check() & _BV(BTN1))
+				btns.cntrs[0]++;
+			else
+				rt |= _BV(BTN1);
+		}
+	} else if (pressed & _BV(BTN1)) {
+		btns.cntrs[0]++;
+	}
+	if (btns.cntrs[1]) {
+		btns.cntrs[1]++;
+		if (!btns.cntrs[1]) {
+			if (BTN_check() & _BV(BTN2))
+				btns.cntrs[1]++;
+			else
+				rt |= _BV(BTN2);
+		}
+	} else if (pressed & _BV(BTN2)) {
+		btns.cntrs[1]++;
+	}
+	if (btns.cntrs[2]) {
+		btns.cntrs[2]++;
+		if (!btns.cntrs[2]) {
+			if (BTN_check() & _BV(BTN3))
+				btns.cntrs[2]++;
+			else
+				rt |= _BV(BTN3);
+		}
+	} else if (pressed & _BV(BTN2)) {
+		btns.cntrs[2]++;
+	}
+	return rt;
 }
 
 /*Place for functions which buttons may trigger*/
-static inline void BTN_perform()
+static inline void BTN_perform(uint8_t action)
 {
-	if (_BV(BTN1) & btns.pressed & (~btns.held)) {
+	if (_BV(BTN1) & action) {
 		LED_OFF();
-		btns.pressed &= ~_BV(BTN1);
 	}
-	if (_BV(BTN2) & btns.pressed & (~btns.held)) {
+	if (_BV(BTN2) & action) {
 		LED_ON();
-		btns.pressed &= ~_BV(BTN2);
 	}
-	if (_BV(BTN3) & btns.pressed & (~btns.held)) {
+	if (_BV(BTN3) & action) {
 		LED_OFF();
-		btns.pressed &= ~_BV(BTN3);
 	}
 }
 
 void BTN_action()
 {
-	btns.pressed |= BTN_check();
-	if (btns.pressed) {
-		BTN_debounce();
-		btns.held = BTN_check() & btns.pressed;
-		BTN_perform();
-	}
+	BTN_perform(BTN_debounce());
 }
 
